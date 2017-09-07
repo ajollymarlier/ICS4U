e1a8907909a6434063a5ca44ccdc7d5362f69ba1
ajollymarlier <ajollymarlier@ajmarliersp3.bayviewglen.ca>
2017-09-07 08:48:59 -0400

    Refactored package

---
+++ Unit 1/src/com/bayviewglen/dayone/DayOne.java
@@ -0,0 +1,29 @@
+package com.bayviewglen.dayone;
+
+import java.awt.Toolkit;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class DayOne {
+
+	public static void main(String[] args) throws InterruptedException {
+		//TODO dont run this unless you want to kill your CPU
+		long beepCount = System.currentTimeMillis();
+		
+		while(true ){
+			if(System.currentTimeMillis() - beepCount > 100) {
+				Toolkit.getDefaultToolkit().beep();
+				beepCount = System.currentTimeMillis();
+			}
+			
+			/*JFrame frame = new JFrame();
+			frame.setSize(100, 100);
+			frame.setLocation(new Random().nextInt(), new Random().nextInt());
+			frame.setVisible(true);*/
+		}
+
+	}
+
+}
