8b52c97e0a3f5770ff72350062dadefef0ad1930
ajollymarlier <ajollymarlier@ajmarliersp3.bayviewglen.ca>
2017-10-03 18:51:12 -0400

    Pc push

--- Unit 1/src/com/bayviewglen/trees/BinarySearchTree.java
+++ Unit 1/src/com/bayviewglen/trees/BinarySearchTree.java
@@ -19,60 +19,105 @@ public class BinarySearchTree {
 	public void setRoot(IntTreeNode root) {
 		this.root = root;
 	}
-	
+
 	public void add(IntTreeNode current, int x) {
 		IntTreeNode node = new IntTreeNode(x);
-		
-		if(root == null) {
-			root = node;
-		}else if(current.getData() > x) {
+
+		if (current.getData() > x && current.getLeft() == null) {
+			current.setLeft(node);
+
+		} else if (current.getData() > x && current.getLeft() != null) {
 			add(current.getLeft(), x);
-		}else if(current.getData() <= x) {
+
+		} else if (current.getData() <= x && current.getRight() == null) {
+			current.setRight(node);
+
+		} else if (current.getData() <= x && current.getRight() != null) {
 			add(current.getRight(), x);
+
 		}
 	}
-	
+
 	public void add(int x) {
-		if(root == null) {
+		if (root != null) {
+			add(root, x);
+		} else {
 			IntTreeNode node = new IntTreeNode(x);
 			root = node;
-		}else {
-			add(root, x);
 		}
 	}
 
 	public void traverseInOrder(IntTreeNode current) {
-		if(current.getLeft() != null)
+		if (current.getLeft() != null)
 			traverseInOrder(current.getLeft());
-		
+
 		evaluate(current);
-		
-		if(current.getRight() != null)
+
+		if (current.getRight() != null)
 			traverseInOrder(current.getRight());
-		
+
 	}
 
 	public void traversePreFix(IntTreeNode current) {
 		evaluate(current);
-		
-		if(current.getLeft() != null)
+
+		if (current.getLeft() != null)
 			traverseInOrder(current.getLeft());
-		
-		if(current.getRight() != null)
+
+		if (current.getRight() != null)
 			traverseInOrder(current.getRight());
 	}
 
 	public void traversePostFix(IntTreeNode current) {
-		if(current.getLeft() != null)
+		if (current.getLeft() != null)
 			traverseInOrder(current.getLeft());
-		
-		if(current.getRight() != null)
+
+		if (current.getRight() != null)
 			traverseInOrder(current.getRight());
-		
+
 		evaluate(current);
 	}
-	
+
 	private void evaluate(IntTreeNode x) {
 		System.out.println(x.getData());
+	}
+	
+	public Integer findSmallestNode(IntTreeNode current) {
+		int data = 0;
+		if(current.getLeft() == null) {
+			return current.getData();
+		}else {
+			data = findSmallestNode(current.getLeft());
+		}
+		
+		return data;
+	}
+	
+	public Integer findLargestNode(IntTreeNode current) {
+		int data = 0;
+		if(current.getRight() == null) {
+			return current.getData();
+		}else {
+			data = findLargestNode(current.getRight());
+		}
+		
+		return data;
+	}
+	
+	//TODO not working
+	public IntTreeNode search(IntTreeNode current, int i) {
+		IntTreeNode node = null;
+		
+		if(i < current.getData()) {
+			search(current.getLeft(), i);
+			
+		}else if(i > current.getData()) {
+			search(current.getRight(), i);
+			
+		}else if(i == current.getData()) {
+			node = current;
+		}
+		
+		return node;
 	}
 }
--- Unit 1/src/com/bayviewglen/trees/Driver.java
+++ Unit 1/src/com/bayviewglen/trees/Driver.java
@@ -3,7 +3,22 @@ package com.bayviewglen.trees;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BinarySearchTree bst = new BinarySearchTree();
+		
+		bst.add(4);
+		bst.add(34);
+		bst.add(23);
+		bst.add(9);
+		bst.add(1);
+		bst.add(2);
+		bst.add(3);
+		bst.add(2);
+		
+		bst.traverseInOrder(bst.getRoot());
+		System.out.println(bst.findSmallestNode(bst.getRoot()));
+		System.out.println(bst.findLargestNode(bst.getRoot()));
+		
+		System.out.println(bst.search(bst.getRoot(), 9));
 
 	}
 
