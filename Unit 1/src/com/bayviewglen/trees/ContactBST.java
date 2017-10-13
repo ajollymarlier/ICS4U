d254c9c09174c44a1375deeaaa3d855147c002f2
ajollymarlier <ajollymarlier@ajmarliersp3.bayviewglen.ca>
2017-10-13 17:48:55 -0400

    pc push

--- Unit 1/src/com/bayviewglen/trees/ContactBST.java
+++ Unit 1/src/com/bayviewglen/trees/ContactBST.java
@@ -82,15 +82,17 @@ public class ContactBST {
 	}
 	
 	//This is an added method from standard BST API
-	public void saveInOrder(ContactNode current, BSTContact[] arr, int i) throws IOException{
-		if (current.getLeft() != null)
-			saveInOrder(current.getLeft(), arr, i);
-
+	public int saveInOrder(ContactNode current, BSTContact[] arr, int i) throws IOException{
 		arr[i] = current.getData();
 		i++;
+		
+		if (current.getLeft() != null)
+			i = saveInOrder(current.getLeft(), arr, i);
 
 		if (current.getRight() != null)
-			saveInOrder(current.getRight(), arr, i);
+			i = saveInOrder(current.getRight(), arr, i);
+		
+		return i;	
 	}
 
 	private void evaluate(ContactNode x) {
@@ -136,32 +138,47 @@ public class ContactBST {
 		return node;
 	}
 
-	public boolean remove(ContactNode parent, String key) {
+	public boolean remove(ContactNode parent, String lNameKey) {
 		if (parent == null) {
 			return false;
 		}
 
 		boolean found = false;
-
-		if (parent.getLeft() != null && key.equals(parent.getLeft().getData().getLname())) {
-			found = deleteNode(parent, parent.getLeft(), 0);
-			found = true;
-
-		} else if (parent.getRight() != null && key.equals(parent.getLeft().getData().getLname())) {
-			found = deleteNode(parent, parent.getRight(), 1);
-			found = true;
+		
+		//TODO this root thing doesnt work either
+		if(lNameKey.equals(root.getData().getLname())){
+			found = deleteRoot();
 			
-		} else if (key.compareTo(parent.getData().getLname()) <= -1) {
-			found = remove(parent.getLeft(), key);
+		} else if (parent.getLeft() != null && lNameKey.equals(parent.getLeft().getData().getLname())) {
+			found = deleteNode(parent, parent.getLeft(), 0);
 
-		} else if (key.compareTo(parent.getData().getLname()) >= 1) {
-			found = remove(parent.getRight(), key);
+			//TODO check the parent. stuff
+		} else if (parent.getRight() != null && lNameKey.equals(parent.getRight().getData().getLname())) {
+			found = deleteNode(parent, parent.getRight(), 1);
+			
+		} else if (lNameKey.compareTo(parent.getData().getLname()) <= -1) {
+			found = remove(parent.getLeft(), lNameKey);
+
+		} else if (lNameKey.compareTo(parent.getData().getLname()) >= 1) {
+			found = remove(parent.getRight(), lNameKey);
 
 		}
 
 		return found;
 	}
 
+	//TODO work on this
+	private boolean deleteRoot() {
+		if(root.getRight() != null) {
+			ContactNode temp = root;
+			root = temp.getRight();
+			root.getRight().setLeft(root.getLeft());
+			root.setLeft(temp.getLeft());
+		}
+		
+	}
+
+	//TODO this doesnt actually work if removed child also has stuff
 	// For Direction of Child: 0 = left, 1 = right
 	private boolean deleteNode(ContactNode parent, ContactNode child, int direction) {
 
@@ -174,11 +191,12 @@ public class ContactBST {
 		} else if (child.getLeft() == null && child.getRight() == null && direction == 1) {
 			parent.setRight(null);
 
+			//TODO this doesnt work
 			// Works for both directions, but copies data of right tree to child then sets
 			// right tree link to null
 		} else if (child.getRight() != null) {
-			child.setData(child.getRight().getData());
-			child.setRight(null);
+			child.setData(findLargestNode(child.getLeft()));
+			remove(findLargestNode(child.getLeft()));
 
 			// sets parent left link to child left link
 		} else if (child.getLeft() != null && direction == 0) {
