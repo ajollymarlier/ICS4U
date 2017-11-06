7e10144877c4174a00169be09c4c8a46c3df9b3e
ajollymarlier <ajollymarlier@bayviewglen.ca>
2017-11-05 19:15:48 -0500

    Laptop push. Neighbours still not working

--- Unit 2 - Algorithims/src/com/bayviewglen/dpproblemset/HateYourNeighbour.java
+++ Unit 2 - Algorithims/src/com/bayviewglen/dpproblemset/HateYourNeighbour.java
@@ -5,20 +5,30 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class HateYourNeighbour {
-	static int n = 10;
+	static int n = 8;
 	static int[] nums = readFile();
 	static int[] sols = new int[n + 1];
 
 	public static void main(String[] args) {
-		sols[1] = nums[1];
-		for(int i = 1; i <= n; i+=2) {
-			sols[i] = sols[i - 1] + nums[i];
-			
-			
+
+		if (n == 1)
+			System.out.println(nums[1]);
+		else if (n == 2)
+			System.out.println(Math.max(nums[1], nums[2]));
+		else if (n == 3)
+			System.out.println(Math.max(Math.max(nums[1], nums[2]), nums[3]));
+		else {
+			sols[0] = 0;
+			sols[1] = nums[1];
+
+			for (int i = 2; i <= n; i+=2) {
+				if()
+
+			}
 		}
 
 	}
-	
+
 	private static int[] readFile() {
 		Scanner scanner;
 		int[] nums = null;
