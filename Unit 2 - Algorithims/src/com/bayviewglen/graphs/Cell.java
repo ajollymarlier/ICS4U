3bf2a6f4727a8bf83ab6c636734d956366bf1a8e
ajollymarlier <ajollymarlier@ajmarliersp3.bayviewglen.ca>
2017-11-07 14:11:15 -0500

    Graph Push

---
+++ Unit 2 - Algorithims/src/com/bayviewglen/graphs/Cell.java
@@ -0,0 +1,52 @@
+package com.bayviewglen.graphs;
+
+public class Cell {
+	private int row;
+	private int col;
+	private int translation;
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+	
+	public void setCol(int col) {
+		this.col = col;
+	}
+	
+	public Cell(int row, int col) {
+		this.row = row;
+		this.col = col;
+		this.translation = 0;
+	}
+	
+	public Cell(int row, int col, int translation) {
+		this.row = row - translation;
+		this.col = col - translation;
+		this.translation = translation;
+	}
+	
+	public boolean equals(Object o){
+	    if (o instanceof Cell) { 
+	    	return ((Cell)o).row == row && ((Cell)o).col == col;
+	    }
+	    return false;
+	}
+	
+	public String toString(){
+		return "("+(row + translation) + ", " + (col + translation) + ")";
+	}
+	
+	
+	public int hashCode(){
+		return Integer.valueOf(row).hashCode() + Integer.valueOf(col).hashCode();
+	}
+	
+}
---
+++ Unit 2 - Algorithims/src/com/bayviewglen/graphs/Cell_3D.java
@@ -0,0 +1,44 @@
+package com.bayviewglen.graphs;
+
+public class Cell_3D extends Cell {
+
+	private int z;
+	private int translation;
+	
+	public Cell_3D(int row, int col) {
+		super(row, col);
+		this.z = 0;
+	}
+	
+	public Cell_3D(int x, int y, int z){
+		super(x, y);
+		this.z = z;
+	}
+	
+	public Cell_3D(int x, int y, int z, int translation){
+		super(x, y);
+		this.z = z;
+		this.translation = translation;
+	}
+	
+	public boolean equals(Object o){
+	    if (o instanceof Cell) { 
+	    	return ((Cell_3D)o).getRow() == getRow() && ((Cell_3D)o).getCol() == getCol() && ((Cell_3D)o).z == z;
+	    }
+	    
+	    return false;
+	}
+	
+	public String toString(){
+		return "("+(getRow()+translation) + ", " + (getCol()+translation) + ", " + (z+translation) + ")";
+	}
+	
+	
+	public int hashCode(){
+		return Integer.valueOf(getRow()).hashCode() + Integer.valueOf(getCol()).hashCode() + Integer.valueOf(z).hashCode();
+	}
+
+	public int getZ(){
+		return this.z;
+	}
+}
