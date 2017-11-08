b9a20d5a7a1686f7f31d2d2f756ace4100a042ab
ajollymarlier <ajollymarlier@bayviewglen.ca>
2017-11-07 19:39:51 -0500

    Fixed Subsequence bug and displayed sequence

--- Unit 2 - Algorithims/src/com/bayviewglen/dpproblemset/ContigiousSubSequence.java
+++ Unit 2 - Algorithims/src/com/bayviewglen/dpproblemset/ContigiousSubSequence.java
@@ -6,38 +6,58 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ContigiousSubSequence {
-	static int n = 7;
+	static int n;
 	static int maxSum = 0;
 	static int[] nums = readFile();
 	static int[] sols = new int[n + 1];
+	static ArrayList<Integer> maxCombo = new ArrayList<Integer>();
+	static ArrayList<Integer> currCombo = new ArrayList<Integer>();
+	
 
-	//TODO need to output numbers
 	public static void main(String[] args) {
 		sols[0] = 0;
-		
-		for(int i = 1; i <= n; i++){
-			if(!(nums[i] + sols[i - 1] <= 0)) {
-				sols[i] = nums[i] + sols[i - 1];
-			}else {
+
+		for (int i = 1; i <= n; i++) {
+			if (nums[i] < 0 && sols[i - 1] > maxSum) {
+				maxCombo = (ArrayList<Integer>) currCombo.clone();
+				currCombo.clear();
 				maxSum = sols[i - 1];
 			}
+				
+			if (!(nums[i] + sols[i - 1] <= 0)) {
+				currCombo.add(nums[i]);
+				sols[i] = nums[i] + sols[i - 1];
+				
+			} else {
+				if (sols[i] > maxSum){
+					maxCombo = (ArrayList<Integer>) currCombo.clone();
+					currCombo.clear();
+					maxSum = sols[i - 1];
+				}
+					
+			}
 		}
-		
-		if(sols[n] > maxSum)
+
+		if (sols[n] > maxSum)
 			maxSum = sols[n];
 		
-		System.out.println("Sum is " + maxSum);
+		for(int i = 0; i < maxCombo.size(); i++){
+			System.out.print(maxCombo.get(i) + ", ");
+		}
+
+		System.out.println("\nMax sum is " + maxSum);
 
 	}
-	
+
 	private static int[] readFile() {
 		Scanner scanner;
 		int[] nums = null;
 		try {
 			scanner = new Scanner(new File("data/subsequence.dat"));
+			n = scanner.nextInt();
 			nums = new int[n + 1];
 			for (int i = 1; i <= n; i++) {
-				nums[i] = Integer.parseInt(scanner.nextLine().trim());
+				nums[i] = scanner.nextInt();
 			}
 
 			scanner.close();
@@ -47,5 +67,4 @@ public class ContigiousSubSequence {
 
 		return nums;
 	}
-
 }
