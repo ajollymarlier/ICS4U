f07321c13f5348b983d9dae49bc9bad371b6f889
Arun Jolly Marlier <ajollymarlier@bayviewglen.ca>
2018-01-31 14:10:47 -0500

    Actually have a plan now... sort of

--- JQuery/js/data.js
+++ JQuery/js/data.js
@@ -1,21 +1,78 @@
+$(document).ready(function(){
+
 var horses = [{name: "Aliqyan", img: createImage("images/delo.png"), x: 0}, {name: "Rossos", img: createImage("images/delo.png"), x: 0},
               {name: "Horse", img: createImage("images/delo.png"), x: 0}, {name: "XQC", img: createImage("images/delo.png"), x: 0}];
 
-var walletAmt = 1000;
+var wallet = 1000;
+//Add a dialog at the beginning of game to add name
+var playerName = 'Arun';
 
-var information = $('#information');
 var raceArea = $('#race');
-var betMenu = $('#betMenu');
-var horseInfo = $('#horseInfo');
-
-
+var bettingMenu = $('#betMenu');
+var playerTable = $('playerTable');
 
 raceArea.hide();
-betMenu.hide();
-horseInfo.tabs();
+bettingMenu.hide();
+
+document.getElementById("raceButton").addEventListener("click", race);
+document.getElementById("betButton").addEventListener("click", betMenu);
+//TODO this one doesnt link to anything
+updateTable();
+
+//document.getElementById("horse1Att").getElementsByTagName('p').innerHTML = 	horses[0];
+
+function updateTable(){
+	$('#playerTable tbody').remove();
+	$('#playerTable').append('<tbody><tr><td>' + playerName + '</td><td>$' + wallet + '</td></tr></tbody>');
+}
 
 function createImage(src){
     var tmp = new Image();
     tmp.src = src;
     return tmp;
 }
+
+function betMenu(){
+	var betInput = $('#betAmount').spinner();
+	betInput.spinner({min: 0, max: wallet, step: 10});
+	bettingMenu.dialog({modal: true, resizable: false, title: 'Betting Window', buttons: {"Bet": makeBet}});
+	bettingMenu.show();
+}
+
+//if you type as value higher than max, it still works
+function makeBet(){
+	if($('#betAmount').spinner('value') > wallet)
+		$('#betAmount').val(wallet);
+
+	wallet -= $('#betAmount').spinner('value');
+	$('#betAmount').val(0);
+	updateTable();
+	bettingMenu.dialog('close');
+
+}
+
+function race(){
+	if($('#race').is(":visible")){
+		$('#race').hide();
+	}else{
+		$('#race').show();
+	}
+
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext('2d');
+
+//Waitz 30 milliseconds for image to load then calls draw Horses
+	setTimeout(function (){drawHorses(canvas, ctx);}, 30)
+
+	function drawHorses(canvas, ctx){
+ 	var startY = 0;
+  	ctx.beginPath();
+  	for(var i = 0; i < horses.length; i++){
+    	ctx.drawImage(horses[i].img, 10, startY, 100, 100);
+    	startY += 100;
+  		}
+  		ctx.closePath();
+	}
+}
+
+});
--- JQuery/js/race.js
+++ JQuery/js/race.js
@@ -1,23 +1,5 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext('2d');
-
-/*ctx.beginPath();
-ctx.rect(30, 30, 30, 30);
-ctx.fillStyle = 'red';
-ctx.fill();
-ctx.closePath();
-*/
-
-//Waitz 30 milliseconds for image to load then calls draw Horses
-setTimeout(function (){drawHorses(canvas, ctx);}, 30)
+$(document).ready(function(){
 
 
-function drawHorses(canvas, ctx){
-  var startY = 0;
-  ctx.beginPath();
-  for(var i = 0; i < horses.length; i++){
-    ctx.drawImage(horses[i].img, 10, startY, 100, 100);
-    startY += 100;
-  }
-  ctx.closePath();
-}
+
+});
