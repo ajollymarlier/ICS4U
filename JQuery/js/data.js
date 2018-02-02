e22892c1e062cb6a3954e178ebd848edbd2f943f
Arun Jolly Marlier <ajollymarlier@bayviewglen.ca>
2018-02-02 15:37:36 -0500

    Need to add game loop and update tables after race

--- JQuery/js/data.js
+++ JQuery/js/data.js
@@ -12,6 +12,7 @@ var raceArea = $('#race');
 var bettingMenu = $('#betMenu');
 var nameMenu = $('#nameMenu');
 var playerTable = $('playerTable');
+var horseTable = $('raceHorseInfo');
 
 raceArea.hide();
 bettingMenu.hide();
@@ -19,35 +20,68 @@ nameMenu.hide();
 
 document.getElementById("raceButton").addEventListener("click", race);
 document.getElementById("betButton").addEventListener("click", betMenu);
-document
+
 getName();
 setRaceHorses();
-
+updateHorseTable();
+	
+//TODO need to find which horse is checked
 function setRaceHorses(){
     var numHorses = Math.floor(Math.random() * 4) + 4;
 
     for(var i = 0; i < numHorses; i++){
-      raceHorses[i] = horses[Math.floor(Math.random() * horses.length)];
+      //Creates object with horse name and bet
+      //will need to adjust x value to fit with start of track
+      var temp = {name: horses[Math.floor(Math.random() * horses.length)], bet: 0, x: 0};
+
+      if(!contains(temp, raceHorses))
+      	raceHorses[i] = temp;
+      else
+      	i--;
+    }
+
+    for(var i = 0; i < raceHorses.length; i++){
+    	$("#betMenu form").append('<input type="radio" name="horseSelect" value=' + i +'><label>'+ raceHorses[i].name +'</label><br>');
     }
 }
 
+//Returns if array contains element
+function contains(element, arr){
+	for(var i = 0; i < arr.length; i++){
+		if(arr[i].name === element.name)
+			return true;
+	}
+
+	return false;
+}
+
+function updateHorseTable(){
+	$('#horseTable tbody').remove();
+
+	for(var i = 0; i < raceHorses.length; i++){
+		$('#horseTable').append('<tbody><tr><td>' + raceHorses[i].name + '</td><td>$' + raceHorses[i].bet + '</td></tr></tbody>');
+	}
+}
+
 function getName(){
-	nameMenu.dialog({closeOnEscape: false, resizable: false, title: 'Player Name', buttons: {"Start": addName}});
+	nameMenu.dialog({closeOnEscape: false, resizable: false, title: 'Enter a name for your player', buttons: {"Start": addName}, open: function(event, ui) {
+        $(".ui-dialog-titlebar-close", ui.dialog | ui).fadeOut('slow');
+    }});
 	//TODO enter thing still not working but it does work in betMenu
-	nameMenu.keyPress(function(event){
+	/*nameMenu.keyPress(function(event){
 		if (event.keyCode === 13) //13 is enter
         	event.preventDefault();
- 	 })
-	nameMenu.show();
+ 	 });*/	
+	nameMenu.fadeIn('slow');
 }
 
 function addName(){
 	playerName = $('#nameInput').val();
-	updateTable();
+	updatePlayerTable();
 	nameMenu.dialog('close');
 }
 
-function updateTable(){
+function updatePlayerTable(){
 	$('#playerTable tbody').remove();
 	$('#playerTable').append('<tbody><tr><td>' + playerName + '</td><td>$' + wallet + '</td></tr></tbody>');
 }
@@ -56,23 +90,75 @@ function betMenu(){
 	var betInput = $('#betAmount').spinner({min: 0, max: wallet, step: 10});
 	bettingMenu.dialog({closeOnEscape: false, resizable: false, title: 'Betting Window', buttons: {"Bet": makeBet}});
 
-	//TODO esc still exiting, enter works tho
 	bettingMenu.keypress(function(event){
 		if (event.keyCode === 13) //13 is enter
         	event.preventDefault();
  	 });
-	bettingMenu.show();
+	bettingMenu.fadeIn('slow');
 }
 
-//if you type as value higher than max, it still works
 function makeBet(){
 	if($('#betAmount').spinner('value') > wallet)
 		$('#betAmount').val(wallet);
 
 	wallet -= $('#betAmount').spinner('value');
+	var betIndex = $('input[name="horseSelect"]:checked').val();
+	raceHorses[betIndex].bet += $('#betAmount').spinner('value');
+
 	$('#betAmount').val(0);
-	updateTable();
+	updatePlayerTable();
+	updateHorseTable();
 	bettingMenu.dialog('close');
 }
+
+function race(){
+    if(!$('#race').is(":visible")){
+     	$('#race').fadeIn('slow');
+    }
+
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext('2d');
+
+    while(!raceFinished()){
+    	for(var i = 0; i < raceHorses.length; i++){
+    		raceHorses[i].x += Math.floor(Math.random() * 10);
+    	}
+
+    	//Waitz 30 milliseconds for image to load then calls draw Horses
+    	//setTimeout(function(){drawHorses(canvas, ctx);}, 30);
+
+	    function drawHorses(canvas, ctx){
+	      var startY = 0;
+	     // ctx.beginPath();
+	      for(var i = 0; i < horses.length; i++){
+	       // ctx.drawImage("horseSprites.png", 10, startY, 100, 100);
+	        startY += 100;
+	      } 
+	      //ctx.closePath();
+	    }
+    }
+
+    var winningHorse = {x: 0};
+    for(var i = 0; i < raceHorses.length; i++){
+    	if(raceHorses[i].x > winningHorse.x)
+    		winningHorse = raceHorses[i];
+    }
+
+    if(winningHorse.bet > 0)
+    	wallet += winningHorse.bet * 2;
+
+    setTimeout(function(){raceArea.hide();}, 1000);
+  }
+
+  function raceFinished(){
+  	var maxX = 300; //Temp value. Will adjust for size of track.
+
+  	for(var i = 0; i < raceHorses.length; i++){
+  		if(raceHorses[i].x >= maxX)
+  			return true;
+  	}
+
+  	return false;
+  }
 
 });
--- JQuery/js/race.js
+++ JQuery/js/race.js
@@ -1,27 +1,5 @@
 $(document).ready(function(){
-  function race(){
-    if($('#race').is(":visible")){
-      $('#race').fadeOut('slow');
-    }else{
-      $('#race').fadeIn('slow');
-    }
-
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext('2d');
-
-  //Waitz 30 milliseconds for image to load then calls draw Horses
-    setTimeout(function (){drawHorses(canvas, ctx);}, 30)
-
-    function drawHorses(canvas, ctx){
-      var startY = 0;
-      ctx.beginPath();
-      for(var i = 0; i < horses.length; i++){
-        ctx.drawImage(horses[i].img, 10, startY, 100, 100);
-        startY += 100;
-      } 
-      ctx.closePath();
-    }
-  }
+  
 
   function moveHorses(){
 
