5b2f9506ca08577dfa02a0d184152a9c1a355fff
Arun Jolly Marlier <ajollymarlier@bayviewglen.ca>
2018-08-03 18:28:51 -0400

    PC OusI

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,10 +1,24 @@
+//Creates module for page
 var app = angular.module("flapperNews", []);
 
+//Defines controller for page
+//This contains all the $scope variables that will be called by the HTML
 app.controller('MainCtrl', [
 	'$scope',
 	function($scope){
-		$scope.test = "Hello World";
+		$scope.test = "Please";
 
 		$scope.posts = [
-		'1', '2', '3', '4'];
+		{title: 'Title 1', upvotes: 4},
+		{title: 'Title 2', upvotes: 12},
+		{title: 'Title 3', upvotes: 3},
+		{title: 'Title 4', upvotes: 6},
+		{title: 'Title 5', upvotes: 5},
+		];
+
+		$scope.addPost = function(){
+			if(!$scope.title || $scope.title === "") {return;}
+			$scope.posts.push({title: $scope.title, upvotes: 0});
+			$scope.title = '';
+		};
 	}]);
