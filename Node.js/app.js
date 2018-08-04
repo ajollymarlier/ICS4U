0c256fbf0b90066ab3bf8b589802d2343a9c29ba
ajollymarlier <ajollymarlier@bayviewglen.ca>
2018-08-03 23:06:03 -0400

    Added link and title adding by user input

--- Node.js/app.js
+++ Node.js/app.js
@@ -18,7 +18,12 @@ app.controller('MainCtrl', [
 
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === "") {return;}
-			$scope.posts.push({title: $scope.title, upvotes: 0});
+			$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
 			$scope.title = '';
+			$scope.link = '';
+		};
+
+		$scope.incrementUpvotes = function (post){
+			post.upvotes += 1;
 		};
 	}]);
